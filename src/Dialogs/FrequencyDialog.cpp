5b0810210363c44cad4f9351833f1242611cf006
Dave Spragg <dave.spragg@gmail.com>
2023-02-05 15:51:15 +0000

    .

--- src/Dialogs/FrequencyDialog.cpp
+++ src/Dialogs/FrequencyDialog.cpp
@@ -83,7 +83,7 @@ public:
     // Draw name and frequency
     row_renderer.DrawTextRow(canvas, rc, channel.name.c_str());
 
-//    if (channel.radio_frequency.IsDefined()) {
+    if (channel.radio_frequency.IsDefined()) {
     	StaticString<30> buffer;
     	TCHAR radio[20];
       channel.radio_frequency.Format(radio, ARRAY_SIZE(radio));
@@ -91,7 +91,7 @@ public:
       row_renderer.DrawRightColumn(canvas, rc, buffer);
 //      row_renderer.NextColumn(canvas, rc, buffer);
 //      row_renderer.DrawRightColumn(canvas, rc, _("1234"));
-//      }
+      }
 
   }
 
@@ -167,8 +167,8 @@ FrequencyListWidget::UpdateList() noexcept
 	  if (name == nullptr)
 	    continue;
 	  const TCHAR *frequency = i->GetAttribute(_T("frequency"));
-	  if (frequency == nullptr)
-	    continue;
+//	  if (frequency == nullptr)
+//	    continue;
 	  const TCHAR *squawk = i->GetAttribute(_T("squawk"));
 
 	  RadioFrequency radio_frequency = RadioFrequency::Parse(frequency);
