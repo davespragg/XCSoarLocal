1b3dccf6c4b591bab13f599c772313c7b5eb4def
Dave Spragg <dave.spragg@gmail.com>
2023-02-05 16:18:37 +0000

    .

--- src/Dialogs/FrequencyDialog.cpp
+++ src/Dialogs/FrequencyDialog.cpp
@@ -109,8 +109,10 @@ FrequencyListWidget::CreateButtons(WidgetDialog &dialog)
     unsigned index = GetCursorIndex();
     assert(index < channels->size());
     const RadioChannel *channel = &(*channels)[index];
-    ActionInterface::SetStandbyFrequency(channel->radio_frequency,
+    if (channel->radio_frequency.IsDefined()) {
+    	ActionInterface::SetStandbyFrequency(channel->radio_frequency,
             channel->name.c_str());
+    }
     cancel_button->Click();
 });
 
